acc747629ad14c399a41bf201541009b5413f353
Ian <ianbraverman@hotmail.com>
2024-03-22 12:52:34 -0400

    got the home page nav links working with a ternery operator for logged in or not. Also, did pick quiz page starting shell to check if they have user topics using a hypothetical get from the database to get user topics associated with the token. Then, it also gets the names of the topic associated with the topic id.

--- src/client/features/dashboard/dashboardSlice.js
+++ src/client/features/dashboard/dashboardSlice.js
@@ -1,8 +1,10 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import api from "../../store/api";
+import { selectToken } from "../auth/authSlice";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
 
 const dashboardAPI = api.injectEndpoints({
-    endpoints: builder = ({})
+  endpoints: (builder = {}),
 });
 
 export const {} = dashboardAPI;
