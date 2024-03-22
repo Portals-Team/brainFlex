c61a900054d25b53c8f850689faa72b6ac722a83
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-22 09:16:27 -0500

    created basic file setup for accountSlice

--- src/client/features/dashboard/dashboardSlice.js
+++ src/client/features/dashboard/dashboardSlice.js
@@ -1,9 +1,1 @@
-import api from "../../store/api";
 
-//endpoints unique to api documentation
-
-const dashboardApi = api.injectEndpoints({
-  endpoints: (builder) => ({}),
-});
-
-export const {} = dashboardApi;
