9ac7eaccb16b55d046de50c3889f965c6ad37f3d
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-22 14:31:21 -0500

    completed comments for database fetches for Quiz component

--- src/client/features/game/Quiz.jsx
+++ src/client/features/game/Quiz.jsx
@@ -1,3 +1,11 @@
+/*Current question will fetch from the database Quiz_problems to show the tally of how many questions have been answered in the quiz*/
+
+/*Question will fetch from the database via the Question table to display the question*/
+
+/*Question answers will be fetched from the database via the Question table to display all the possible answers for the question*/
+
+/*Score will be fetched from the database via the Quiz_problems table to display a tally of how many questions the players got wrong or right*/
+
 export default function Quiz() {
   return (
     <>
@@ -6,23 +14,26 @@ export default function Quiz() {
       <section>
         <li>Current Question</li>
         <div>
-          <h1>QUESTION:</h1>
+          <h3>
+            QUESTION: this is the current question fetched from the database
+          </h3>
           <ol>
+            {/*function-upon entering an answer the user will be directed to the quiz answer page*/}
             <li>
-              <input type="checkbox" id="scales" name="scales" />
-              <label for="scales">ANSWER A</label>
+              <input type="radio" id="answerA" name="answer" />
+              <label for="answerA">ANSWER A</label>
             </li>
             <li>
-              <input type="checkbox" id="scales" name="scales" />
-              <label for="scales">ANSWER B</label>
+              <input type="radio" id="answerB" name="answer" />
+              <label for="answerB">ANSWER B</label>
             </li>
             <li>
-              <input type="checkbox" id="scales" name="scales" />
-              <label for="scales">ANSWER C</label>
+              <input type="radio" id="answerC" name="answer" />
+              <label for="answerC">ANSWER C</label>
             </li>
             <li>
-              <input type="checkbox" id="scales" name="scales" />
-              <label for="scales">ANSWER D</label>
+              <input type="radio" id="answerD" name="answer" />
+              <label for="answerD">ANSWER D</label>
             </li>
           </ol>
         </div>
