54ca0eeda4d3de30cea71e56cde65e6464c3e13f
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-03-22 12:25:50 -0400

    Wrote out the base code for ChooseTopics, and created new file TopicItem to display the topics of each category.  Code is all commented out to avoid errors until useGetCategoriesQuery and UseGetCategoryTopicsQuery are written

--- src/client/features/account/ChooseTopics.jsx
+++ src/client/features/account/ChooseTopics.jsx
@@ -1,9 +1,21 @@
+/*
+import {useGetCategoriesQuery} from "../game/gameSlice"
 
 export default function ChooseTopics() {
-
+    const {data: categories} = useGetCategoriesQuery();
+    
     return (
-    <>
-        <h1>Choose Topics</h1>
-    </>
-  )
+        <ul className="categoryList">
+          <li>
+            {categories.map(category => (
+              <>
+                <h2>{category.name}</h2>
+                <TopicItem key={category.id} category={category} />
+              </>
+              )
+            )}
+          </li>
+        </ul>
+  );
 }
+*/
