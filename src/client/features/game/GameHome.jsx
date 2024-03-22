3fa686b7df145ca8a4f7dc1f3dd878210450d805
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-22 11:15:16 -0500

    added comments for game functionality

--- src/client/features/game/GameHome.jsx
+++ src/client/features/game/GameHome.jsx
@@ -1,3 +1,13 @@
+/*Current question will pull from the API quiz questions to show the tally of how mant questions have been answered in the quiz*/
+
+/*image will be pulled from the API? to show the image for that quiz*/
+
+/*answer grid will fetch the answer word from the API and reveal a letter when the user correctly answers a quiz question*/
+
+/*the solve button will direct the user to the QuizAnswer page to solve the quiz*/
+
+/*ready for next question button will allow the user to toggle back and forth from Quiz component and the GameHome component*/
+
 export default function GameHome() {
   return (
     <>
@@ -5,6 +15,7 @@ export default function GameHome() {
       {/*flex this section into a row*/}
       <section>
         <li>Current Question</li>
+        {/*image will be blurred and come into focus when a question is answered correctly*/}
         <img src="https://picsum.photos/id/237/200/300" />
         <li>Score</li>
       </section>
