05ba7cfe792752dc65c8c943faa0e06851a30451
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-22 11:05:27 -0500

    created html structure for GameHome component

--- src/client/features/game/GameHome.jsx
+++ src/client/features/game/GameHome.jsx
@@ -1,9 +1,32 @@
-
 export default function GameHome() {
-
-    return (
+  return (
     <>
-        <h1>GameHome</h1>
+      <h1>GameHome</h1>
+      {/*flex this section into a row*/}
+      <section>
+        <li>Current Question</li>
+        <img src="https://picsum.photos/id/237/200/300" />
+        <li>Score</li>
+      </section>
+      <section>
+        {/*answer grid should be a controlled form*/}
+        <h1>ANSWER GRID</h1>
+        <form>
+          {/*in CSS resize the width of each input container to be the length 
+          of one letter*/}
+          <div>
+            <input type="text" maxlength="1" class="letter" />
+            <input type="text" maxlength="1" class="letter" />
+            <input type="text" maxlength="1" class="letter" />
+            <input type="text" maxlength="1" class="letter" />
+            <input type="text" maxlength="1" class="letter" />
+          </div>
+        </form>
+      </section>
+      <section>
+        <button>SOLVE</button>
+        <button>READY FOR NEXT QUESTION</button>
+      </section>
     </>
-  )
+  );
 }
