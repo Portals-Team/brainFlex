67091b30b23c55bcd7ca82e4692cd065b5fd7266
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-22 09:12:32 -0500

    basic file setup for dashboardApi

---
+++ src/client/features/dashboard/dashboardSlice.js
@@ -0,0 +1,9 @@
+import api from "../../store/api";
+
+//endpoints unique to api documentation
+
+const dashboardApi = api.injectEndpoints({
+  endpoints: (builder) => ({}),
+});
+
+export const {} = dashboardApi;
