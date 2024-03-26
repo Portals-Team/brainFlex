cd5db67e741b9bd0e623149d49f200afeee81f7e
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-03-26 10:44:57 -0400

    Updated patch method for quiz_problem to match error throws

--- src/server/api/quiz_problems/index.js
+++ src/server/api/quiz_problems/index.js
@@ -19,20 +19,32 @@ router.patch("/:id", async (req, res, next) => {
     const { user_answer } = req.body;
     const { id } = req.params;
     try {
+      if(!res.locals.user) {
+        return next({
+            status: 400,
+            message: "You are not logged into the correct account"
+        });
+      }
+      
       const quizProblemId = +id;
+      if (error.code === "P2025") {
+        return next({
+          status: 404,
+          message: `Quiz problem with id ${id} not found.`
+        });
+      } 
       const updateProblem = await prisma.quiz_problems.update({
         where: { id: quizProblemId },
         data: { user_answer },
       });
+      if (!updateProblem) {
+        return next({
+          status: 500,
+          message: "An error occurred while updating the quiz problem."
+        });
+      }
       res.json(updateProblem);
     } catch (error) {
-      if (error.code === "P2025") {
-        res.status(404).send(`Quiz problem with id ${id} not found.`);
-      } else {
-        console.error(error);
-        res
-          .status(500)
-          .send("An error occurred while updating the quiz problem.");
-      }
+      next(error);
     }
 });
