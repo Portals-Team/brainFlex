1269854fe9a64d689e705423a081d4f09fe38dfc
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-28 07:45:00 -0500

    create function to display topics and logic to return user topics

--- src/client/features/account/UserStats.jsx
+++ src/client/features/account/UserStats.jsx
@@ -5,24 +5,19 @@ import { useGetUserTopicsQuery } from "./accountSlice";
 
 function TopicCard({ topic }) {
   return (
-    <section>
-      <li>
-        <h4>{topic.name}</h4>
-      </li>
-    </section>
+    <li>
+      <h4>{topic.name}</h4>
+    </li>
   );
 }
 
 export default function UserStats() {
   const { id } = useParams();
   const { data: user } = useGetUserQuery(id);
-  console.log(user);
 
   const { data: topics } = useGetTopicsQuery();
-  console.log(topics);
 
   const { data: userTopics } = useGetUserTopicsQuery(id);
-  console.log(userTopics);
 
   //we've got user topics but each user topic does not have a name and needs to refer to the topics table for its name.
 
@@ -33,24 +28,35 @@ export default function UserStats() {
   //   const topicById = topicsdata.id;
   //   return topicById.name;
   // }
+  const userTopicPicks = userTopics?.map((userTopics) => {
+    return topics?.find((topics) => topics.id === userTopics.topic_id);
+  });
 
   return (
     <>
       <p>User Stats!</p>
       <div>
-        {/* Example: Display user information */}
         <h1>Welcome! {user?.name}</h1>
         <p>Username: {user?.username}</p>
         <p>Aggregate Score: {user?.aggregate_score}</p>
         <p>Quiz Count: {user?.quiz_count}</p>
-        {/* Add more user details here */}
       </div>
-      <h2>Topics:</h2>
-      <ul>
-        {topics?.map((topic) => (
-          <TopicCard key={topic.id} topic={topic} />
-        ))}
-      </ul>
+      <div>
+        <h2>Topics:</h2>
+        <ul>
+          {topics?.map((topic) => (
+            <TopicCard key={topic.id} topic={topic} />
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>User Topics: </h2>
+        <ul>
+          {userTopicPicks?.map((topic) => (
+            <TopicCard key={topic.id} topic={topic} />
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
