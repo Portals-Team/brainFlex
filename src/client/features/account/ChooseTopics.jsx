fdf405285e43099828c747d8489974c9e0d50999
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-28 12:52:38 -0500

    added checkboxes to choose topics list, limited user to 3 choices functionality

--- src/client/features/account/ChooseTopics.jsx
+++ src/client/features/account/ChooseTopics.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 import { useGetCategoriesQuery } from "./accountSlice";
 
-//create a checkbox for each topic
-//let a user only select three topics
 //user can sumbit topics and it will update their acoount PATCH
+//create a patch request, if nothing is there patch if something is there then update.
 export default function ChooseTopics() {
   const { data: categories } = useGetCategoriesQuery();
+  const [selectedTopics, setSelectedTopics] = useState([]);
 
-  function CategoryItem({ category }) {
+  const handleCheckChange = (topicId, isChecked) => {
+    if (isChecked) {
+      if (selectedTopics.length < 3) {
+        setSelectedTopics([...selectedTopics, topicId]);
+      } else {
+        alert("You can only choose 3 topics");
+      }
+    } else {
+      setSelectedTopics((prevSelected) =>
+        prevSelected.filter((id) => id !== topicId)
+      );
+    }
+  };
+
+  function CategoryItem({ category, onCheckboxChange, selectedTopics }) {
     return (
       <div>
         <h3>{category.name}</h3>
@@ -15,7 +29,12 @@ export default function ChooseTopics() {
           {category.Category_topics.map(({ topic }) => (
             <li key={topic.id}>
               <label>
-                <input type="checkbox" value={topic.id} />
+                <input
+                  type="checkbox"
+                  value={topic.id}
+                  checked={selectedTopics.includes(topic.id)}
+                  onChange={(e) => onCheckboxChange(topic.id, e.target.checked)}
+                />
                 {topic.name}
               </label>
             </li>
@@ -32,10 +51,16 @@ export default function ChooseTopics() {
         <h2>Categories:</h2>
         <ul>
           {categories?.map((category) => (
-            <CategoryItem key={category.id} category={category} />
+            <CategoryItem
+              key={category.id}
+              category={category}
+              onCheckboxChange={handleCheckChange}
+              selectedTopics={selectedTopics}
+            />
           ))}
         </ul>
       </div>
+      <p>Submit Topics</p>
     </>
   );
 }
