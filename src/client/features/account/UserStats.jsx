d07582ba7788b895c89bfd1fd55a212277a8a489
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-28 09:42:09 -0500

    fixed top score bug in UserStats component

--- src/client/features/account/UserStats.jsx
+++ src/client/features/account/UserStats.jsx
@@ -4,9 +4,14 @@ import { useGetTopicsQuery } from "./accountSlice";
 import { useGetUserTopicsQuery } from "./accountSlice";
 import { useGetUsersQuery } from "./accountSlice";
 
-/*create logic to render top players by their aggregate score
- *const sortedScores = users?.aggregate_scores.sort(a, b) => b -a );
- *const topThreeScores = sortedScores.slice(0, 3)*/
+function TopicCard({ topic }) {
+  return (
+    <li>
+      <h4>{topic.name}</h4>
+    </li>
+  );
+}
+
 function UserScores({ user }) {
   return (
     <ul>
@@ -16,34 +21,16 @@ function UserScores({ user }) {
   );
 }
 
-function TopicCard({ topic }) {
-  return (
-    <li>
-      <h4>{topic.name}</h4>
-    </li>
-  );
-}
-
 export default function UserStats() {
   const { id } = useParams();
-
   const { data: users } = useGetUsersQuery();
-  console.log(users);
-
   const { data: user } = useGetUserQuery(id);
-
   const { data: topics } = useGetTopicsQuery();
-
   const { data: userTopics } = useGetUserTopicsQuery(id);
 
   const userTopicPicks = userTopics?.map((userTopics) => {
     return topics?.find((topics) => topics.id === userTopics.topic_id);
   });
-
-  /*const sortedUsers = [...users].sort(
-    (a, b) => b.aggregate_score - a.aggregate_score
-  );
-  const topPlayers = sortedUsers?.slice(0, 3);*/
 
   return (
     <>
@@ -77,11 +64,12 @@ export default function UserStats() {
       </div>
       <div>
         <h3>TOP PLAYERS:</h3>
-        {/*getUsers if users.aggregate score is in the top 3 return those players and there score*/}
         <ul>
-          {users?.map((user) => (
-            <UserScores key={user.id} user={user} />
-          ))}
+          {users?.length > 0 &&
+            [...users]
+              .sort((a, b) => b.aggregate_score - a.aggregate_score)
+              .slice(0, 3)
+              .map((user) => <UserScores key={user.id} user={user} />)}
         </ul>
       </div>
     </>
