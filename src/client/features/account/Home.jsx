1ab771428632a581538ea814d8aefb9c77605b6e
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-29 13:27:15 -0500

    created main home page Home.jsx in account folder

---
+++ src/client/features/account/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  //   this page will show an image with a game graphic and a button to play game, if the user is logged in the user will be directed to the PickQuizPage and if is not logged in they will be directed to the AuthForm page
+
+  return (
+    <>
+      <h1>Main Landing Page</h1>
+    </>
+  );
+}
