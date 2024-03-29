fa4750448482316ffbfce29ccb698e1d3b557f63
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-29 15:15:27 -0500

    added some more wireframe styling

--- src/client/features/account/PickQuizPage.jsx
+++ src/client/features/account/PickQuizPage.jsx
@@ -7,7 +7,7 @@ function TopicCard({ topic }) {
   return (
     <li>
       <h4>{topic?.name}</h4>
-      <button>Play {topic?.name} Quiz</button>
+      <button>Play Quiz</button>
       {/* make this button a Link tag to generated quiz for the users picked topic*/}
     </li>
   );
@@ -22,13 +22,15 @@ export default function PickQuizPage() {
       <h1>Pick Quiz Page</h1>
       <div>
         <h2>Your Topics: </h2>
-        <ul>
-          {user?.user_topics?.map(({ Topics }) => (
-            <TopicCard key={Topics?.id} topic={Topics} />
-          ))}
-          {/* make this button a Link tag to generated random quiz for the users picked topic*/}
-          <button>Play Random Quiz</button>
-        </ul>
+        <div>
+          <ul id="yourTopics">
+            {user?.user_topics?.map(({ Topics }) => (
+              <TopicCard key={Topics?.id} topic={Topics} />
+            ))}
+          </ul>
+        </div>
+        {/* make this button a Link tag to generated random quiz for the users picked topic*/}
+        <button>Play Random Quiz</button>
       </div>
     </>
   );
