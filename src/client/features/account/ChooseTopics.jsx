6644b80f8d6603dd15c080a610a50827d14106c0
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-30 16:45:51 -0500

    completed styling on choose topics page

--- src/client/features/account/ChooseTopics.jsx
+++ src/client/features/account/ChooseTopics.jsx
@@ -38,13 +38,14 @@ export default function ChooseTopics() {
 
   function CategoryItem({ category, onCheckboxChange, selectedTopics }) {
     return (
-      <div>
-        <h3>{category.name}</h3>
+      <div id="categoryCard">
+        <h3 id="categoryName">{category.name}</h3>
         <ul>
           {category.Category_topics.map(({ topic }) => (
             <li key={topic.id}>
-              <label>
+              <label id="topicsNames">
                 <input
+                  id="checkbox"
                   type="checkbox"
                   value={topic.id}
                   checked={selectedTopics.includes(topic.id)}
@@ -61,9 +62,10 @@ export default function ChooseTopics() {
 
   return (
     <>
-      <h2>Pick 3 Favorite Quiz Topics</h2>
+      <p id="chooseTopicsHeadline">Favorite Quiz Topics:</p>
+      <p id="chooseTopicsTag">Pick 3</p>
       <div>
-        <ul id="pickTopics">
+        <ul>
           {categories?.map((category) => (
             <CategoryItem
               key={category.id}
@@ -74,7 +76,7 @@ export default function ChooseTopics() {
           ))}
         </ul>
       </div>
-      <button onClick={handleSumbit}>
+      <button id="submitTopicsButton" onClick={handleSumbit}>
         <Link to={`/account/${id}`}>Submit Topics</Link>
       </button>
     </>
