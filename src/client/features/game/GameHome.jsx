555c25aee86711342447db9d68617ac7cdb138bc
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-04-01 16:32:11 -0400

    added some JSDoc to GameHome

--- src/client/features/game/GameHome.jsx
+++ src/client/features/game/GameHome.jsx
@@ -8,9 +8,13 @@ import { useState } from "react";
 
 import "./game.css";
 
+/**
+ * 
+ * @returns GameHome react component, where the quiz lives throughout
+ */
 export default function GameHome() {
   const { id } = useParams();
-  const { data: quiz } = useGetGameQuery(+id);
+  const { data: quiz } = useGetGameQuery(+id); 
   const { data: image_word } = useGetImageWordQuery(quiz?.image_Word_id);
   const numberOfCorrectQuestions = numberOfAnswersCorrect();
   const navigate = useNavigate();
@@ -22,12 +26,21 @@ export default function GameHome() {
   let acc = 1;
   const [userInput, setUserInput] = useState(Array(gameWord?.length).fill(""));
 
+  /**
+   * Function sets specific values of user inputted strings for the word guess
+   * @param {Integer} index index of the user input word guess
+   * @param {String} value specific letter at each point in word guess
+   */
   const handleInputChange = (index, value) => {
     const updatedInput = [...userInput];
     updatedInput[index] = value;
     setUserInput(updatedInput);
   };
 
+  /**
+   * 
+   * @returns an Integer of how many questions that the user has answered correctly
+   */
   function numberOfAnswersCorrect() {
     let numberOfCorrectQuestions = 0;
     for (let i = 0; i < 10; i++) {
@@ -41,6 +54,11 @@ export default function GameHome() {
     return numberOfCorrectQuestions+1;
   }
 
+  /**
+   * 
+   * @param {Integer} numberOfCorrectQuestions 
+   * @returns a String of how many letters to reval based on how many questions the user has correctly anwswered
+   */
   function showRevealedLetters(numberOfCorrectQuestions) {
     let revealedLetters = "";
     for (let i = 0; i < numberOfCorrectQuestions - 1; i++) {
@@ -49,6 +67,11 @@ export default function GameHome() {
     return revealedLetters;
   }
 
+  /**
+   * Helper function that concats the strings of what is revealed and known against what the user guesses
+   * @param {String} guessWord 
+   * @returns a boolean if the word guessed by the user is correct or not.
+   */
   function isGuessCorrect(guessWord) {
     return (
       (
@@ -57,6 +80,10 @@ export default function GameHome() {
     );
   }
 
+  /**
+   * Function takes the boolean from isGuessCorrect and patches the users score and routes them to see if their answer is correct or not
+   * @param {String} guessWord 
+   */
   function submitAnswer(guessWord) {
     if (isGuessCorrect(guessWord)) {
       updateAggregateScore();
@@ -64,6 +91,9 @@ export default function GameHome() {
     } else navigate(`/game/score/incorrect/${id}`);
   }
 
+  /**
+   * 
+   */
   const updateAggregateScore = async () => {
     await updateUser({
       id: quiz?.user_id,
@@ -71,6 +101,9 @@ export default function GameHome() {
     }).unwrap();
   };
 
+  /**
+   * 
+   */
   return (
     <>
       <section id="imageContainer">
