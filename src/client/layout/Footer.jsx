5c4b06628cefd895d6c4d87ed3cebdc9f61c1e8e
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-04-02 13:43:42 -0500

    wrote JSDOC for footer and added functional links for creators

--- src/client/layout/Footer.jsx
+++ src/client/layout/Footer.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 
 import "./navbar.css";
 
+/**
+ *
+ * @component Footer universal component via Root.jsx can be viewed on every page of the application with links to home, account, game tutorial and indicates if a user is logged in or logged out.
+ */
 export default function Footer() {
   return (
     <section id="footerSection">
@@ -19,7 +23,30 @@ export default function Footer() {
       <Link to="howtoplay/1">
         <h3 id="howToPlay">how to play </h3>
       </Link>
-      <p id="creators">creators: ian / kyle / rich</p>
+      <div id="creators">
+        <p className="creators">creators: </p>
+        <a
+          className="creators"
+          target="_blank"
+          href="https://www.linkedin.com/in/bravermanian/"
+        >
+          Ian Braverman
+        </a>
+        <a
+          className="creators"
+          target="_blank"
+          href="https://www.linkedin.com/in/kyle-cogan-268aa1a9/"
+        >
+          Kyle Cogan
+        </a>
+        <a
+          className="creators"
+          target="_blank"
+          href="https://www.linkedin.com/in/richmarafioti/"
+        >
+          Rich Marafioti
+        </a>
+      </div>
     </section>
   );
 }
