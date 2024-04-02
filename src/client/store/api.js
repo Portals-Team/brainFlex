3a7c57a47cf93db0b42503b6f26a68640e34e7ac
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-04-02 14:01:27 -0500

    wrote JSDOC for api file

--- src/client/store/api.js
+++ src/client/store/api.js
@@ -1,9 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 /**
- * Empty central API slice.
- * Endpoints should be injected in their own slices.
- * If available, an auth token is added to all request headers.
+ * @description Empty central API slice. Endpoints should be injected in their own slices. If available, an auth token is added to all request headers.
  */
 const api = createApi({
   baseQuery: fetchBaseQuery({
